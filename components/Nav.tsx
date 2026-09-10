f9fc3c4e84c8f68177cb30418d14dfbfee64f3aa
Babayemi Enoch Marvelous <mbabayemi.2501784@stu.cu.edu.ng>
2026-09-10 20:48:50 +0100

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,7 +8,7 @@ const LINKS = [
   { href: "/overview", label: "Overview", icon: Globe2 },
   { href: "/dashboard", label: "Today", icon: LayoutDashboard },
   { href: "/academics", label: "Academics", icon: BookOpenText },
-  { href: "/screentime", label: "Screen time", shortLabel: "Screen", icon: Hourglass },
+  { href: "/screentime", label: "Screen time", shortLabel: "Usage", icon: Hourglass },
   { href: "/finance", label: "Finance", icon: Wallet },
   { href: "/journal", label: "Journal", icon: NotebookPen },
   { href: "/routines", label: "Routines", icon: Repeat2 },
